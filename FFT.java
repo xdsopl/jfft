c6477f40b67120d53fe11f2b83cb419a99353997
Ahmet Inan <xdsopl@gmail.com>
2025-02-21 12:33:41 +0100

    only allow power of two

--- FFT.java
+++ FFT.java
@@ -9,6 +9,8 @@ public class FFT {
 	private Complex tmp0, tmp1;
 
 	FFT(int length) {
+		if (length < 2 || !isPowerOfTwo(length))
+			throw new IllegalArgumentException("FFT length must be a power of 2 and at least 2, but was: " + length);
 		tf = new Complex[length];
 		for (int i = 0; i < length; ++i) {
 			double x = - (2.0 * Math.PI * i) / length;
@@ -20,6 +22,10 @@ public class FFT {
 		tmp1 = new Complex();
 	}
 
+	private boolean isPowerOfTwo(int n) {
+		return n > 0 && (n & (n - 1)) == 0;
+	}
+
 	private void dft2(Complex out0, Complex out1, Complex in0, Complex in1) {
 		tmp0.set(in0);
 		tmp1.set(in1);
@@ -28,18 +34,14 @@ public class FFT {
 	}
 
 	private void radix2(Complex[] out, Complex[] in, int O, int I, int N, int S, boolean D) {
-		switch (N) {
-			case 1:
-				out[O].set(in[I]);
-				return;
-			case 2:
-				dft2(out[O], out[O + 1], in[I], in[I + S]);
-				return;
+		if (N == 2) {
+			dft2(out[O], out[O + 1], in[I], in[I + S]);
+		} else {
+			radix2(out, in, O, I, N / 2, 2 * S, D);
+			radix2(out, in, O + N / 2, I + S, N / 2, 2 * S, D);
+			for (int k0 = 0, k1 = N / 2; k0 < N / 2; ++k0, ++k1)
+				dft2(out[O + k0], out[O + k1], out[O + k0], out[O + k1].mul(D ? tf[k0 * S] : tmp1.set(tf[k0 * S]).conj()));
 		}
-		radix2(out, in, O, I, N / 2, 2 * S, D);
-		radix2(out, in, O + N / 2, I + S, N / 2, 2 * S, D);
-		for (int k0 = 0, k1 = N / 2; k0 < N / 2; ++k0, ++k1)
-			dft2(out[O + k0], out[O + k1], out[O + k0], out[O + k1].mul(D ? tf[k0 * S] : tmp1.set(tf[k0 * S]).conj()));
 	}
 
 	void forward(Complex[] out, Complex[] in) {
