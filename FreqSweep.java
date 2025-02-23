91e27525771f7ff96980ddd9394023f668cf6d6d
Ahmet Inan <xdsopl@gmail.com>
2025-02-23 14:15:00 +0100

    output power in decibels

--- FreqSweep.java
+++ FreqSweep.java
@@ -13,6 +13,10 @@ public class FreqSweep {
 		for (int acc = 0, freq = -10 * factor; freq <= 10 * factor; acc = (acc + ++freq) % range)
 			if (stft.push(input.polar(1, (acc * 2 * Math.PI) / range)))
 				System.out.println(freq / (double) factor + " "
-					+ stft.power[length - 1] + " " + stft.power[0] + " " + stft.power[1]);
+					+ dB(stft.power[length - 1]) + " " + dB(stft.power[0]) + " " + dB(stft.power[1]));
+	}
+
+	private static double dB(double power) {
+		return 10.0 * Math.log10(power);
 	}
 }
