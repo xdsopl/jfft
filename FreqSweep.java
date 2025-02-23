4de45da81a98d3294da41050129b3443093ff377
Ahmet Inan <xdsopl@gmail.com>
2025-02-23 10:28:57 +0100

    lets look closer

--- FreqSweep.java
+++ FreqSweep.java
@@ -9,8 +9,8 @@ public class FreqSweep {
 		int length = 256;
 		ShortTimeFourierTransform stft = new ShortTimeFourierTransform(length);
 		Complex input = new Complex();
-		int factor = 1000, range = length * factor;
-		for (int acc = 0, freq = -range / 2; freq <= range / 2; acc = (acc + ++freq) % range)
+		int factor = 10000, range = length * factor;
+		for (int acc = 0, freq = -10 * factor; freq <= 10 * factor; acc = (acc + ++freq) % range)
 			if (stft.push(input.polar(1, (acc * 2 * Math.PI) / range)))
 				System.out.println(freq / (double) factor + " "
 					+ stft.power[length - 1] + " " + stft.power[0] + " " + stft.power[1]);
